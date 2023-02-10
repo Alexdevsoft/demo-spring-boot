faead1f97f081912b48c3320e3b438b0f92b2530
Alexdevsoft <alexhavilla2022@gmail.com>
2023-02-10 10:09:53 -0300

    Order part 2

---
+++ src/main/java/com/educando/demo/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.educando.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educando.demo.entities.Order;
+import com.educando.demo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+
+}
