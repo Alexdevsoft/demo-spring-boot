c298d0bbc9b4a0caba18fb4a1a9b253e009a4931
Alexdevsoft <alexhavilla2022@gmail.com>
2023-02-12 10:44:24 -0300

    Exception handling - delete

--- src/main/java/com/educando/demo/services/UserService.java
+++ src/main/java/com/educando/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educando.demo.entities.User;
 import com.educando.demo.repositories.UserRepository;
+import com.educando.demo.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/educando/demo/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.educando.demo.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not Found. Id" + id);
+	}
+
+}
