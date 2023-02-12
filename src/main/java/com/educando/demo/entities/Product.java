b9d7e78ef74521789013cda0c1df3b1bb89c85ac
Alexdevsoft <alexhavilla2022@gmail.com>
2023-02-11 23:47:13 -0300

    Product-OrderItem One-to-Many

--- src/main/java/com/educando/demo/entities/Product.java
+++ src/main/java/com/educando/demo/entities/Product.java
@@ -1,9 +1,12 @@
 package com.educando.demo.entities;
 
 import java.io.Serializable;
+import java.security.cert.X509CertSelector;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -13,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +35,9 @@ public class Product implements Serializable {
 	@ManyToMany(cascade = CascadeType.PERSIST)
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
@@ -86,6 +93,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> setOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
